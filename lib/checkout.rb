a7910f455d6f0c8055101c4620979cdfd50b56cb
Sayem995 <sayem.bokhari.1@gmail.com>
2020-01-05 14:37:47 +0000

    fourth failing test passing

--- lib/checkout.rb
+++ lib/checkout.rb
@@ -2,8 +2,18 @@ class Shop
   def checkout(items)
     return -1 unless items.kind_of? String
     return -1 if items != items.upcase || items =~ /\d/
-
-    calc(items)
+    @cost = 0
+    calc(items.chars)
 
   end
+end
+
+def calc(items)
+  items.each do |item|
+    case item
+    when 'A'
+      @cost += 50
+    end
+  end
+  @cost
 end
